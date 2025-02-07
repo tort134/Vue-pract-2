601618cd227184b993156b32ca4ceadd6c018468
Канаш Тимофей Кириллович <gr424_katki@ttit.local>
2025-02-07 10:44:52 +0700

    add base js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+Vue.component('note-component', {
+    template: `
+        <div class="note"></div>
+        <h3>{{ note.name }}}</h3> 
+        <ul>
+            <li>
+                <input type="checkbox">
+                <span>{{ item.text }}}</span>
+                
+            </li>
+<!--            <div>Complete: {{ note.complate }}}</div>-->
+        </ul>
+    `
+})
+
 new Vue({
     el: '#app',
     data(){
         return{
+            note: [],
+            newNoteName: '',
+            newNoteItems: ['', '', ''],
 
         }
     },
@@ -10,11 +28,11 @@ new Vue({
 
     },
 
-    computed(){
+    computed: {
 
     },
 
-    methods(){
+    methods: {
 
     }
 })
